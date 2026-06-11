a4ec265e1f4764ea38637616bc02bb83cab1ab0b
Robert <robertniyompuhwe@gmail.com>
2026-06-11 05:23:43 +0200

    some debugging

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from './Guards';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
     }),
   );
   app.enableShutdownHooks();
+  app.useGlobalGuards(new AuthGuard())
   const port = 3333;
   await app.listen(port);
   console.log('app is listenin on port' + port);
